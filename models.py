f1936cbf70578fac5fbc22754233a0f90f3c5050
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-02-06 15:15:24 +0100

    Add Client model

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 import enum
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey, DateTime
+from database import Base
+from datetime import datetime
+from sqlalchemy.orm import relationship
 from database import Base
 
 class UserRole(enum.Enum):
@@ -14,8 +17,19 @@ class User(Base):
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
-
     role = Column(Enum(UserRole), nullable=False)
 
-    def __repr__(self):
-        return f"<User {self.username} ({self.role.value})>"
+
+class Client(Base):
+    __tablename__ = 'clients'
+
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    phone = Column(String, nullable=True)
+    company_name = Column(String, nullable=False)
+
+    creation_date = Column(DateTime, default=datetime.now)
+    last_update = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+    commercial_contact_id = Column(Integer, ForeignKey('users.id'), nullable=False)
--- test.py
+++
@@ -1,9 +0,0 @@
-from database import engine
-
-try:
-    connection = engine.connect()
-    print("Connexion réussie ")
-    connection.close()
-except Exception as e:
-    print(f"Erreur : {e}")
-
