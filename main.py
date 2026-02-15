502250490b11508fed5d6509b826e4f7b0c467a0
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-02-15 08:46:39 +0100

    setup main

--- main.py
+++ main.py
@@ -1,32 +1,63 @@
 import questionary
-from controllers.auth import login_user
+import os
+from controllers.auth import login_user, get_logged_user
+
 
 def main():
-    print("\n EPIC EVENTS CRM \n")
+    print("\n EPIC EVENTS CRM  \n")
 
     while True:
-        choix = questionary.select(
-            "Que voulez-vous faire ?",
-            choices=[
-                "Se connecter",
-                "Quitter"
-            ]
-        ).ask()
+        # On vérifie si un User est déja connecté
+        user = get_logged_user()
 
-        if choix == "Se connecter":
-            username = questionary.text("Nom d'utilisateur :").ask()
-            password = questionary.password("Mot de passe :").ask()
+        if user:
+            print(f"\n Connecté : {user['username']} - Rôle : {user['role']}")
 
-            token = login_user(username, password)
-
-            if token:
-                print("\n Vous êtes connecté.")
+            # On vérifie son role pour personnaliser le menu
+            if user['role'] == "Gestion":
+                choix = questionary.select(
+                    "Menu Principal :",
+                    choices=["Gérer les Collaborateurs", "Se déconnecter", "Quitter"]
+                ).ask()
             else:
-                print("\n Identifiants incorrects.")
+                choix = questionary.select(
+                    "Menu Principal :",
+                    choices=["Se déconnecter", "Quitter"]
+                ).ask()
 
-        elif choix == "Quitter":
-            print("Au revoir ")
-            break
+            # Menu des gestiionnaires
+            if choix == "Gérer les Collaborateurs":
+                menu_gestion_utilisateurs()
+
+            # Autre options
+            elif choix == "Se déconnecter":
+                if os.path.exists(TOKEN_FILE):
+                    os.remove(TOKEN_FILE)
+                print("Au revoir !")
+
+            elif choix == "Quitter":
+                print("Fermeture du programme.")
+                break
+
+        # Cas ou personne est connecté
+        else:
+            choix = questionary.select(
+                "Accueil :",
+                choices=["Se connecter", "Quitter"]
+            ).ask()
+
+            if choix == "Se connecter":
+                identifiant = questionary.text("Identifiant :").ask()
+                mot_de_passe = questionary.password("Mot de passe :").ask()
+
+                token = login_user(identifiant, mot_de_passe)
+
+                if token:
+                    print(" Connexion réussie !")
+                else:
+                    print(" Erreur : Identifiant ou mot de passe incorrect.")
+
+
 
 if __name__ == "__main__":
     main()
