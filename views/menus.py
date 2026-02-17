c81acc244704f07fc5b5300ebaf372264888687c
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-02-17 18:58:21 +0100

    add menu

--- views/menus.py
+++ views/menus.py
@@ -1,5 +1,6 @@
 import questionary
 from controllers.users import create_user, get_all_users, update_user, delete_user
+from controllers.clients import create_client, get_all_clients, update_client
 from views.user_view import display_all_users
 
 def menu_gestion_utilisateurs():
@@ -64,4 +65,44 @@ def menu_gestion_utilisateurs():
                 print("Annulation.")
 
         elif action == "Retour":
+            break
+
+def menu_gestion_clients(user_id):
+    while True:
+        choix = questionary.select(
+            "GESTION CLIENTS :",
+            choices=["Nouveau Client", "Voir les Clients", "Modifier Client", "Retour"]
+        ).ask()
+
+        # Creation d'un client
+        if choix == "Nouveau Client":
+            nom = questionary.text("Nom :").ask()
+            email = questionary.text("Email :").ask()
+            tel = questionary.text("Téléphone :").ask()
+            entreprise = questionary.text("Nom de l'entreprise :").ask()
+
+            if create_client(nom, email, tel, entreprise, user_id):
+                print("Client créé")
+            else:
+                print("Erreur")
+
+        # Get All clients
+        elif choix == "Voir les Clients":
+            clients = get_all_clients()
+            print("\n--- LISTE DES CLIENTS ---")
+            for client in clients:
+                print(f"{client.company_name} | Contact: {client.full_name} | Commercial ID: {client.commercial_contact_id}")
+            print("-------------------------\n")
+
+        # Update clients
+        elif choix == "Modifier Client":
+            nom_cible = questionary.text("Nom du client à modifier :").ask()
+            nouvelle_boite = questionary.text("Nouveau nom d'entreprise :").ask()
+
+            if update_client(nom_cible, nouvelle_boite, user_id):
+                print("Client modifié.")
+            else:
+                print("Échec modification")
+
+        elif choix == "Retour":
             break
