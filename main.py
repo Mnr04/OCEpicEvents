fc366b21fee19b61688852b8fd5eb111418dc765
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-02-23 16:38:57 +0100

    cli structuration

--- main.py
+++ main.py
@@ -1,84 +1,13 @@
-import questionary
-import os
-from controllers.auth import login_user, get_logged_user, TOKEN_FILE
-from views.menus import menu_gestion_utilisateurs, menu_gestion_clients, menu_gestion_contrats
-from views.menus import menu_gestion_utilisateurs, menu_gestion_clients, menu_gestion_contrats, menu_gestion_events
+import click
+from controllers.auth import get_logged_user
+from views.cli_auth import auth_commands
+from views.cli_users import user_commands
+from views.cli_clients import client_commands
+from views.cli_contracts import contract_commands
+from views.cli_events import event_commands
 
-def main():
-    print("\n EPIC EVENTS CRM  \n")
+@click.group()
+@click.pass_context
+def cli(ctx):
+    ctx.obj = get_logged_user()
 
-    while True:
-        # On vérifie si un User est déja connecté
-        user = get_logged_user()
-
-        if user:
-            print(f"\n Connecté : {user['username']} - Rôle : {user['role']}")
-
-            # On vérifie son role pour personnaliser le menu
-            if user['role'] == "Gestion":
-                choix = questionary.select(
-                    "Menu Principal :",
-                    choices=["Gérer les Collaborateurs", "Gérer les Contrats", "Se déconnecter", "Quitter"]
-                ).ask()
-
-            elif user['role'] == "Commercial":
-                choix = questionary.select(
-                    "Menu Principal :",
-                    choices=["Gérer les Clients","Gérer les Contrats", "Se déconnecter", "Quitter"]
-                ).ask()
-
-            elif user['role'] == "Support":
-                choix_possibles = ["Gérer les Événements", "Se déconnecter", "Quitter"]
-
-            else:
-                choix = questionary.select(
-                    "Menu Principal :",
-                    choices=["Se déconnecter", "Quitter"]
-                ).ask()
-
-            # Sous Menu Gestion
-            if choix == "Gérer les Collaborateurs":
-                menu_gestion_utilisateurs()
-
-            if choix == "Gérer les Clients":
-                menu_gestion_clients(user['user_id'])
-
-            elif choix == "Gérer les Contrats":
-                menu_gestion_contrats(user['role'], user['user_id'])
-
-            # Autre options
-            elif choix == "Se déconnecter":
-                if os.path.exists(TOKEN_FILE):
-                    os.remove(TOKEN_FILE)
-                print("Au revoir !")
-
-            elif choix == "Quitter":
-                print("Fermeture du programme.")
-                break
-
-            elif choix == "Gérer les Événements":
-                menu_gestion_events(user['role'], user['user_id'])
-
-        # Cas ou personne est connecté
-        else:
-            choix = questionary.select(
-                "Accueil :",
-                choices=["Se connecter", "Quitter"]
-            ).ask()
-
-            if choix == "Se connecter":
-                identifiant = questionary.text("Identifiant :").ask()
-                mot_de_passe = questionary.password("Mot de passe :").ask()
-
-                token = login_user(identifiant, mot_de_passe)
-
-                if token:
-                    print(" Connexion réussie !")
-                else:
-                    print(" Erreur : Identifiant ou mot de passe incorrect.")
-
-            elif choix == "Quitter":
-                break
-
-if __name__ == "__main__":
-    main()
