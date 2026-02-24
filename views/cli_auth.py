f0eb2e887776272ab33809f78da81fa274d877f0
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-02-24 06:12:10 +0100

    cli auth

---
+++ views/cli_auth.py
@@ -0,0 +1,18 @@
+import click
+from controllers.auth import login_user
+
+@click.group()
+def auth_commands():
+    pass
+
+@auth_commands.command(name="login")
+@click.option('--username', prompt='Nom d\'utilisateur', help='Votre identifiant.')
+@click.password_option('--password', prompt='Mot de passe', help='Votre mot de passe.')
+def login(username, password):
+    token = login_user(username, password)
+
+    if token:
+        click.secho(f"Connexion réussie pour {username} !", fg="green")
+    else:
+        click.secho("Identifiants incorrects.", fg="red")
+
